431b7bae7dadc968b7f17c96765393111415aee7
Ankit <a53gau@gmail.com>
2021-12-25 15:19:28 +0530

    server.js added

--- client/src/components/Slider/SliderData.js
+++ client/src/components/Slider/SliderData.js
@@ -7,7 +7,7 @@ const SliderData = [
     },
     {
         image:
-            'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80',
+            'https://images.unsplash.com/photo-1640102953836-5651f5d6b240?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1924&q=80',
         title: "NITA LITERARY CLUB",
         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate nostrum, optio voluptatum"
     },
@@ -19,13 +19,13 @@ const SliderData = [
     },
     {
         image:
-            'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80',
+            'https://images.unsplash.com/photo-1639967294864-8ce4f9a8ef80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
         title: "NITA LITERARY CLUB",
         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate nostrum, optio voluptatum"
     },
     {
         image:
-            'https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+            'https://images.unsplash.com/photo-1458869612855-bb6009d50368?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
         title: "NITA LITERARY CLUB",
         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate nostrum, optio voluptatum"
     },
