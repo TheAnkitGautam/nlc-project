119a7b6024f352047d5138e715c525bae8ef9d2f
Ankit <a53gau@gmail.com>
2021-12-30 00:50:45 +0530

    some bug fixes

--- client/src/components/TeamHeader/TeamHeader.jsx
+++ client/src/components/TeamHeader/TeamHeader.jsx
@@ -1,18 +1,22 @@
 import './teamHeader.css';
 import coord1 from './coord-1.png';
 import Dots from '../TeamCard/Dots';
+import FacultyCord from '../TeamCard/FacultyCord';
 const TeamHeader = () => {
     return (
         <>
             <header className="team-header">
                 <h1 className="team-heading"> <span className='meet-word'>MEET </span>OUR TEAM</h1>
                 <p className="team-intro-para">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+
+                <FacultyCord />
+                
                 <div className="coordinator">
                     <div className="coord-wrap">
                         <div className="coordinator-img"><img src={coord1} alt="" /></div>
                         <div className="coordinator-desc">
-                            <h3 className="coord-name">Dr. Anuradha Nongmaithem</h3>
-                            <h5 className="coord-post">Faculty Coordinator</h5>
+                            <h3 className="coord-name">Anawil Tiwari</h3>
+                            <h5 className="coord-post">President</h5>
                             <div className="team-sm-icon">
                                 <a href="https://fb.com"><i className="team-icon fab fa-instagram" /></a>
                                 <a href="https://fb.com"><i className="team-icon fab fa-linkedin-in" /></a>
@@ -24,8 +28,8 @@ const TeamHeader = () => {
                     <div className="coord-wrap">
                         <div className="coordinator-img"><img src={coord1} alt="" /></div>
                         <div className="coordinator-desc">
-                            <h3 className="coord-name">Anawil Tiwari</h3>
-                            <h5 className="coord-post">President</h5>
+                            <h3 className="coord-name">Tarang Vats</h3>
+                            <h5 className="coord-post">Vice President</h5>
                             <div className="team-sm-icon">
                                 <a href="https://fb.com"><i className="team-icon fab fa-instagram" /></a>
                                 <a href="https://fb.com"><i className="team-icon fab fa-linkedin-in" /></a>
