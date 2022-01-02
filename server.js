55ea2326ca945b0c036af6da300a0a72cd964051
Ankit <a53gau@gmail.com>
2022-01-02 16:09:11 +0530

    some minor style changes

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 
-const PORT =  5000;
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server running on ${ PORT }`);
 })
