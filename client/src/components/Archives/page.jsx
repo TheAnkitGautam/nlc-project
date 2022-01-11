93b8061f855c9d1d59dc48f5ceb19b29c2141ab4
Ankit <a53gau@gmail.com>
2022-01-11 14:51:10 +0530

    archive page improved

--- client/src/components/Archives/page.jsx
+++ client/src/components/Archives/page.jsx
@@ -2,9 +2,14 @@ import CSS from './TeamArchive.module.css';
 
 const Page = () => {
     return (
-        <div className={`${CSS.page} pageLoadAnim`} style={{ background: "linear-gradient(180deg, rgba(245, 107, 47, 0.5) -4.96%, rgba(245, 107, 47, 0.20) -4.95%, rgba(255, 255, 255, 0.15) 95.04%), #FFFFFF" }}>
-            <h1 className={CSS.pageTitle}>NLC 1.0</h1>
-        </div>
+        <>
+            <div className={`${CSS.page} pageLoadAnim`} style={{ background: "linear-gradient(180deg, rgba(245, 107, 47, 0.5) -4.96%, rgba(245, 107, 47, 0.20) -4.95%, rgba(255, 255, 255, 0.15) 95.04%), #FFFFFF" }}>
+                <h1 className={CSS.pageTitle}>NLC 1.0</h1>
+            </div>
+            <div className={`${CSS.page} pageLoadAnim`} style={{ background: "linear-gradient(180deg, rgba(245, 107, 47, 0.5) -4.96%, rgba(245, 107, 47, 0.20) -4.95%, rgba(255, 255, 255, 0.15) 95.04%), #FFFFFF" }}>
+                <h1 className={CSS.pageTitle}>NLC 1.0</h1>
+            </div>
+        </>
     );
 }
 
