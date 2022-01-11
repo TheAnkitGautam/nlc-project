87aee6ef33ddb5757c59662809dcd09f0d79210c
Ankit <a53gau@gmail.com>
2022-01-11 14:51:10 +0530

    archive page improved

--- client/src/components/Archives/TeamArchive.jsx
+++ client/src/components/Archives/TeamArchive.jsx
@@ -1,37 +1,99 @@
 import React, { useState } from 'react'
 import CSS from './TeamArchive.module.css';
-import Page from './page';
-
+import image from './coord-1.png';
 
 const TeamArchive = () => {
 
-    const [page, setPage] = useState(0);
+    const [tab, setTab] = useState(0);
+
+    const handleTabChange = (index) => {
+        setTab(index);
+    }
 
     return (
-        <section style={{ paddingTop: "4rem", height: "100vh" }}>
-            <div className={CSS.wrapper}>
-                {page === 0 &&
-                    <div className={CSS.intro_page}>
-                        <h1>Archives</h1>
+        <section style={{ paddingTop: "70px" }}>
+            <div className={CSS.wrapper} style={tab === 1 ? { backgroundColor: "rgb(94, 163, 236)" } : { backgroundColor: "rgb(246, 68, 68)" }}>
+                <div
+                    onClick={() => handleTabChange(0)}
+                    className={tab === 0 ? `${CSS.main} ${CSS.activeTab}` : `${CSS.main} ${CSS.FadeTop} ${CSS.FadeAnim}`}>
+                    <h3 className={CSS.title}>Archives</h3>
+                    <div className={CSS.mainBox}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sit ipsam facilis exercitationem explicabo sed maxime veritatis. Dignissimos praesentium repellendus eos voluptatum, hic odit minima nemo id et dicta minus!
                     </div>
-                }
-
-                {page === 1 &&
-                    <Page />
-                }
-
-                {page === 2 &&
-                    <Page />
-                }
-                <div className={`${CSS.sideBar}`}
-                    onClick={() => setPage(page === 0 || page === 2 ? 1 : 0)}>
-                    page 1
                 </div>
-                <div className={`${CSS.sideBar}`}
-                    onClick={() => setPage(page === 0 || page === 1 ? 2 : 0)} >
-                    page 2
+
+                <div
+                    onClick={() => handleTabChange(1)}
+                    className={tab === 1 ? `${CSS.sidebar} ${CSS.activeTab} ${CSS.tabAnim}` : `${CSS.sidebar} ${CSS.FadeTop} ${CSS.FadeAnim}`}>
+                    <h3 className={CSS.title}>NLC 2.0</h3>
+                    <div className={CSS.sideBox}>
+                        <div className={CSS.content}>
+                            <div className={CSS.imgBox}>
+                                <div className={CSS.card}>
+                                    <img className={CSS.cardImg} src={image} alt="" srcset="" />
+                                    <h6 className={CSS.name}>Ankit</h6>
+                                    <h6 className={CSS.post}>President</h6>
+                                </div>
+                                <div className={CSS.card}>
+                                    <img className={CSS.cardImg} src={image} alt="" srcset="" />
+                                    <h6 className={CSS.name}>Ankit</h6>
+                                    <h6 className={CSS.post}>President</h6>
+                                </div>
+                                <div className={CSS.card}>
+                                    <img className={CSS.cardImg} src={image} alt="" srcset="" />
+                                    <h6 className={CSS.name}>Ankit</h6>
+                                    <h6 className={CSS.post}>President</h6>
+                                </div>
+                            </div>
+                            <div className={CSS.secondBox}>
+                                <h2 className={CSS.mentionTitle}>Other Members</h2>
+                                <ul className={CSS.mentionList}>
+                                    <li className={CSS.mentionItem}>Ankit</li>
+                                    <li className={CSS.mentionItem}>Ankit</li>
+                                    <li className={CSS.mentionItem}>Ankit</li>
+                                    <li className={CSS.mentionItem}>Ankit</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
+                <div
+                    onClick={() => handleTabChange(2)}
+                    className={tab === 2 ? `${CSS.sidebar} ${CSS.activeTab} ${CSS.tabAnim}` : `${CSS.sidebar} ${CSS.FadeTop} ${CSS.FadeAnim}`}>
+                    <h3 className={CSS.title}>NLC 1.0</h3>
+                    <div className={CSS.sideBox}>
+                        <div className={CSS.content}>
+                            <div className={CSS.imgBox}>
+                                <div className={CSS.card}>
+                                    <img className={CSS.cardImg} src={image} alt="" srcset="" />
+                                    <h6 className={CSS.name}>Ankit</h6>
+                                    <h6 className={CSS.post}>President</h6>
+                                </div>
+                                <div className={CSS.card}>
+                                    <img className={CSS.cardImg} src={image} alt="" srcset="" />
+                                    <h6 className={CSS.name}>Ankit</h6>
+                                    <h6 className={CSS.post}>President</h6>
+                                </div>
+                                <div className={CSS.card}>
+                                    <img className={CSS.cardImg} src={image} alt="" srcset="" />
+                                    <h6 className={CSS.name}>Ankit</h6>
+                                    <h6 className={CSS.post}>President</h6>
+                                </div>
+                            </div>
+                            <div className={CSS.secondBox}>
+                                <h2 className={CSS.mentionTitle}>Other Members</h2>
+                                <ul className={CSS.mentionList}>
+                                    <li className={CSS.mentionItem}>Ankit</li>
+                                    <li className={CSS.mentionItem}>Ankit</li>
+                                    <li className={CSS.mentionItem}>Ankit</li>
+                                    <li className={CSS.mentionItem}>Ankit</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
         </section >
     )
 }
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const app = express();
+const nodemailer = require("nodemailer");
+
+
+app.get('/send', async (req, res) => {
+    async function main() {
+
+        let transporter = nodemailer.createTransport({
+            pool: true,
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            auth: {
+                user: "a99gau@gmail.com",
+                pass: "",
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+
+        let info = await transporter.sendMail({
+            from: 'a99gau@gmail.com',
+            to: "",
+            bcc: "vasedik912@veb34.com",
+            subject: "Hello Hari",
+            text: "<h5>This is an test email send through gmail smtp by Ankit</h5><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            text:"Hello This is Ankit"
+        });
+
+        res.json(info).status(200);
+    }
+    main().catch(console.error);
+})
+
 
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on ${ PORT }`);
+    console.log(`Server running on ${PORT}`);
 })
