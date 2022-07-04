23e0e9397660ff5e79efe0ed8246c8090699ad25
Tanmay Shree <tanmay.25ts@gmail.com>
2022-07-04 19:20:10 +0530

    Achievements Page Added, Navbar Response Modified, Navbar Footer Color Change

---
+++ client/src/components/Achievements/AchievementPage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Achievement.css';
+import { Achievement_Set } from "./AchievementSet";
+
+const AchievementPage = () => {
+  return (
+    <div className="achievementPage pageLoadAnim">
+      <h1 className="team-heading"> <span className='meet-word'>OUR </span>ACHIEVEMENTS</h1>
+      <div className="achievementWrapper">
+        {
+          Achievement_Set.map((Achievement, index) => {
+            return (
+            <div className="achievementCard">
+              <div className="achievementCardFace achievementCardFront">
+                <img src={Achievement.frontImg} alt="" />
+              </div>
+              <div className="achievementCardFace achievementCardBack">
+                <img src={Achievement.backImg} alt="" />
+              </div>
+            </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default AchievementPage;
