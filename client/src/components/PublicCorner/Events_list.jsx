8652956e29f167626e1cc5d9796c9e6f5ceb8233
Tanmay Shree <tanmay.25ts@gmail.com>
2022-07-16 18:22:40 +0530

    Responsiveness Modified

---
+++ client/src/components/PublicCorner/Events_list.jsx
@@ -0,0 +1,40 @@
+const Events_list = [
+  {
+    index: 0,
+    name: "Spill the ink 3.0 - Debate Competition",
+    startDate: "20 January",
+    endDate: "18 September",
+  },
+  {
+    index: 1,
+    name: "Spill the ink 3.0 - Debate Competition",
+    startDate: "20 January",
+    endDate: "18 September",
+  },
+  {
+    index: 2,
+    name: "Spill the ink 3.0 - Debate Competition",
+    startDate: "20 January",
+    endDate: "18 September",
+  },
+  {
+    index: 3,
+    name: "Spill the ink 3.0 - Debate Competition",
+    startDate: "20 January",
+    endDate: "18 September",
+  },
+  {
+    index: 4,
+    name: "Spill the ink 3.0 - Debate Competition",
+    startDate: "20 January",
+    endDate: "18 September",
+  },
+  {
+    index: 5,
+    name: "Spill the ink 3.0 - Debate Competition",
+    startDate: "20 January",
+    endDate: "18 September",
+  },
+];
+
+export default Events_list;
