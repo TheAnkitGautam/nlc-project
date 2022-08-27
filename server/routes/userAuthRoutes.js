04715dbae731a7a22accf648c1e426f4a33f3b86
Ankit Gautam <a53gau@gmail.com>
2022-08-27 15:19:59 +0530

    updated passport config

--- server/routes/userAuthRoutes.js
+++ server/routes/userAuthRoutes.js
@@ -10,7 +10,7 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
-router.get('/google/redirect', passport.authenticate('google', {
+router.get('/google/callback', passport.authenticate('google', {
     successRedirect: `${process.env.CLIENT_URL}`,
     failureRedirect: `${process.env.CLIENT_URL}`
 })
