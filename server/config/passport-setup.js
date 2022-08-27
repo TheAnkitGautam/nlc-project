7677b7256481895f751d501b439402965c6bfb12
Ankit Gautam <a53gau@gmail.com>
2022-08-27 15:16:47 +0530

    updated passport config

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -8,7 +8,7 @@ passport.use(
         // options for google strategy
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/redirect'
+        callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
 
         // check if user already exists in our own db
