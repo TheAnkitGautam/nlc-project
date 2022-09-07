19e0aef0b11050f089c74fb3935c2a982ab3091e
Ankit Gautam <a53gau@gmail.com>
2022-09-08 00:48:37 +0530

    local merge

--- server/models/user_profile.js
+++ server/models/user_profile.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-
-const userProfileSchema = new mongoose.Schema({
-    googleId: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    fullName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    profilePic: {
-        type: String,
-    },
-    eventList: {
-        type: Array,
-        default: []
-    },
-    gender: {
-        type: String
-    },
-    whatsappNumber: {
-        type: String
-    },
-    instaUrl: {
-        type: String
-    },
-    instituteName: {
-        type: String
-    }
-},
-    { timestamps: true }
-)
-
-=======
 const mongoose = require('mongoose');
 
 const userProfileSchema = new mongoose.Schema({
@@ -82,5 +41,4 @@ const userProfileSchema = new mongoose.Schema({
     { timestamps: true }
 )
 
->>>>>>> 83abdb41f583fa44adc1c2fc29f853ed52b448fc
 module.exports = mongoose.model("Userprofile", userProfileSchema)
